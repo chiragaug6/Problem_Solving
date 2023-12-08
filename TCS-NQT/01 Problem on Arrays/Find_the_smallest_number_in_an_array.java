a0aed21fdb3d08bde08c7277ce043c1d3fdd8a89
Chirag-Solanki6804 <crs279836@gmail.com>
2023-12-08 21:15:34 +0530

    Array problems

---
+++ TCS-NQT/01 Problem on Arrays/Find_the_smallest_number_in_an_array.java
@@ -0,0 +1,25 @@
+/*
+Problem Statement: Given an array, we have to find the smallest element in the array.
+
+Examples:
+
+Example 1:
+Input: arr[] = {2,5,1,3,0};
+Output: 0
+Explanation: 0 is the smallest element in the array.
+
+Example2:
+Input: arr[] = {8,10,5,7,9};
+Output: 5
+Explanation: 5 is the smallest element in the array.
+
+*/
+
+public class Find_the_smallest_number_in_an_array {
+    public static void main(String[] args) {
+     
+
+
+
+    }
+}
